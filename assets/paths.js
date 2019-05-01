b3fc0460855f0e5218e49dbccfbcb5834b104a89
Vidur Butalia <vidurbutalia@gmail.com>
2019-05-01 13:25:39 +0530

    Add JS skeleton and update paths.js

---
+++ assets/paths.js
@@ -0,0 +1,87 @@
+// paths.js
+// configuration for gulpfile.js
+
+var paths = {};
+
+// Config
+paths.config = {};
+paths.config.minify_suffix = '.min';
+
+
+// Directories
+paths.directories = {};
+paths.directories.build = 'build/';
+
+
+
+//
+// Tasks
+//
+paths.tasks = {
+  css: {
+    default: {
+      source: 'css/main.css',
+      destination: 'style.css',
+      tailwind_config: 'tailwind.js',
+      watch: [
+        'css/**/*.{less,css}',
+        'tailwind.js',
+      ],
+    },
+  },
+  purge: {
+    default: {
+      source: paths.directories.build + 'style.css',
+      destination: 'style.purged.css',
+      config: {
+        content: [
+          '../templates/**/*.{twig,html}',
+          paths.directories.build + '**/*.js',
+        ],
+        extractors: [{
+          extractor: class TailwindExtractor {
+            static extract(content) {
+              return content.match(/[A-Za-z0-9-_:\/]+/g) || [];
+            }
+          },
+          extensions: ["html", "twig", "js"],
+        }],
+      },
+      watch: [
+        paths.directories.build + 'style.css',
+        paths.directories.build + '**/*.js',
+        '../templates/**/*.{twig,html}',
+      ],
+      watch_config: {
+        ignored: '../templates/_manifest.json',
+      },
+    },
+  },
+  js: {
+    urgent: {
+      source: [
+        'js/urgent.js'
+      ],
+      destination: 'urgent.js',
+      watch: [
+      ],
+    },
+    deferred: {
+      source: [
+        'js/deferred.js'
+      ],
+      destination: 'deferred.js',
+      watch: [
+      ],
+    },
+  },
+  hash: {
+    source: paths.directories.build + '**/*.{js,css}',
+    destination: 'manifest.json',
+    watch: paths.directories.build + '**/*.{js,css}',
+  }
+};
+
+
+
+module.exports = paths;
